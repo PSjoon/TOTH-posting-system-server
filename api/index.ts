7f3bec9db5d60013b2ecc34e0451fffeddb1d165
PSjoon <pedrosantos.joon@gmail.com>
2024-02-18 18:18:13 -0300

    add second test

--- api/index.ts
+++ api/index.ts
@@ -8,7 +8,7 @@ const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3334
 
 app.get("/", async (req, res) => {
   console.log("hello World")
-  res.json("Hello World!")
+  res.json(`Hello World in port ${port}`)
 })
 
 try {
