c06893aebe288df9358d4cb8b9ad38eaf3f9c045
PSjoon <pedrosantos.joon@gmail.com>
2024-02-18 18:08:16 -0300

    add test servar application

---
+++ api/index.ts
@@ -0,0 +1,20 @@
+import dotenv from "dotenv"
+import express from "express"
+
+const app = express()
+dotenv.config({ path: ".env" })
+dotenv.config({})
+const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3334
+
+app.get("/", async (req, res) => {
+  console.log("hello World")
+  res.json("Hello World!")
+})
+
+try {
+  app.listen(port, () => {
+    console.log(`Servidor iniciado na porta ${port}`)
+  })
+} catch (error) {
+  console.log(error)
+}
