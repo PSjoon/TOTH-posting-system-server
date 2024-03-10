8f824da730006a69ee0bc0274250ce7891a77766
PSjoon <pedrosantos.joon@gmail.com>
2024-03-10 20:14:35 -0300

    alter index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,13 +1,13 @@
-import dotenv from "dotenv"
-import express from "express"
-import bodyParser from "body-parser"
-import { createArticle } from "./createArticle"
-import { viewArticle } from "./viewArticle"
-import cors from "cors"
+import dotenv from 'dotenv'
+import express from 'express'
+import bodyParser from 'body-parser'
+import { createArticle } from './createArticle'
+import { viewArticle } from './viewArticle'
+import cors from 'cors'
 
 const app = express()
 app.use(cors())
-dotenv.config({ path: "../.env" })
+dotenv.config({ path: '../.env' })
 dotenv.config({})
 
 const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3334
@@ -18,9 +18,9 @@ app.use(bodyParser.json())
 createArticle(app)
 viewArticle(app)
 
-app.get("/", async (req, res) => {
-  console.log("hello World")
-  res.json("Hello World!")
+app.get('/', async (req, res) => {
+  console.log('hello World')
+  res.json('Hello World!')
 })
 
 try {
